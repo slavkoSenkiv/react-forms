34b6aa50ae58110c1d6236d075b3d0dbb279b7d1
Yaroslav Senkiv <slavko.senkiv@gmail.com>
2024-03-19 20:44:07 +0100

    use custom hook to make validation reusable and code leaner

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import RefLogin from './components/RefLogin.jsx';
 import StateLogin from './components/StateLogin.jsx';
 import Signup from './components/Signup.jsx';
 import ComponentLogin from './components/ComponentLogin';
+import CompHookLogin from './components/CompHookLogin.jsx';
 
 function App() {
   return (
     <>
       <Header />
       <main>
+        <CompHookLogin />
         <Signup />
         <ComponentLogin />
         <RefLogin />
